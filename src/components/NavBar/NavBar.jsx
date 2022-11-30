9fe00320dd244a6b062eeed7c312579bf738645f
3DMaX123 <3dmax214@gmail.com>
2022-12-01 00:32:02 +0200

    NavBar - зроблено Footer - в розробці Додав картинки, що потрібні в розробці NavBar, footer не чіпав Index.css -  налаштував під нас та додав декілька root - правил App.js - Налаштував під роботу, готово до використання App.css - видалив все непотрібне, налаштував під нашу роботу

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './NavBar.css';
+import images from '../../constants/images';
+
+class NavBar extends React.Component {
+
+    render(){
+        return (
+            <div className='app__navbar'>
+                <div className='navbar__buttons_helpToy'>
+                    <img className='navbar__buttons_helptoy-icon navbar__helpToyIcon' src={images.navbar__helpToy} alt='Bear with toy'/>
+                    <p className='navbar__buttons_helptoy-text'>допомогти іграшкою</p>
+                </div>
+                <div className='navbar__buttons_charity'>
+                    <p className='navbar__buttons_helptoy-text'>підтримати проєкт</p>
+                    <img className="navbar__buttons_helptoy-icon navbar__charityIcon" src={images.navbar__charity} alt="Charity heart"/>
+                </div>
+            </div>
+          )
+    }
+}
+
+export default NavBar
