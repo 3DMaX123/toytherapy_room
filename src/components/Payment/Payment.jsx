3f2dae9b76618f9357cba168c47838a239621fc4
WebElefont <121201929+WebElefont@users.noreply.github.com>
2023-11-24 17:41:55 +0200

    Зробив сторінку з реквізитами (#8)

---
+++ src/components/Payment/Payment.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import images from '../../constants/images';
+
+import './Payment.css';
+
+class Payment extends React.Component {
+
+  render() {
+
+    return (
+      <div className='app__payment'>
+        <div className='payment__backGround'></div>
+        <div className='payment__content payment__text'>
+          <div className='payment__content_title'>
+            <p className='payment__title-title'>Підтримати проєкт</p>
+            <p className='payment__title-description'>Використовуючи ці реквізити Ви можете допогти команді проєкту розвиватися <br/>
+              надалі і відкривати кімнати з іграшками в інших містах
+            </p>
+          </div>
+          <div className='payment__content_button payment__text-content'>
+            <div className='payment__button_name payment__button'>
+              <p className='payment__name-title payment__text-title'>Найменування отримувача</p>
+              <a className='payment__name-content'>5556 7453 9822 6453</a>
+            </div>
+            <div className='payment__button_code payment__button'>
+              <p className='payment__code-title payment__text-title'>Код отримувача</p>
+              <a className='payment__code-content'>2717819678</a>
+            </div>
+            <div className='payment__button_account payment__button'>
+              <p className='payment__account-title payment__text-title'>Рахунок отримувача</p>
+              <a className='payment__account-content'><a>UA</a>633052990000926002006817905</a>
+            </div>
+            <div className='payment__content_bankName payment__button'>
+              <p className='payment__bankName-title payment__text-title'>Назва банку</p>
+              <a className='payment__bankName-content'> АТ КБ "ПРИВАТБАНК"</a>
+            </div>
+          </div>
+          <img />
+          <img className='payment__content-gradient' src={images.payment__backGround_gradient} alt='img' />
+          <img className='payment__content-backGround' src={images.payment__backGround_cloud} alt='img'/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Payment
